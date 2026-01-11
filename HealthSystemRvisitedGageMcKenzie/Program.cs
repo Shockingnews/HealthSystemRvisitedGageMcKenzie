15774d8752769968280649aa742e6f6997bdd4c4
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-01-11 15:35:38 -0400

    layout

---
+++ HealthSystemRvisitedGageMcKenzie/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemRvisitedGageMcKenzie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    class Health
+    {
+        int _CurrentHealth;
+        int _MaxHealth;
+
+        public void TakeDamage(int damage)
+        {
+
+        }
+        public void Restore()
+        {
+
+        }
+
+        public void Heal(int heal)
+        {
+
+        }
+        Health(int maxHealth)
+        {
+
+        }
+    }
+
+    class Player
+    {
+
+    }
+}
---
+++ HealthSystemRvisitedGageMcKenzie/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HealthSystemRvisitedGageMcKenzie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HealthSystemRvisitedGageMcKenzie")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac6d5002-5e96-4abb-800c-a01b52b334f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
