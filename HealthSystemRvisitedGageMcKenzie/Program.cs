c752893eecf88f808f4dcd52943a6de770bc45d8
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-01-13 14:33:23 -0400

    systems

--- HealthSystemRvisitedGageMcKenzie/Program.cs
+++ HealthSystemRvisitedGageMcKenzie/Program.cs
@@ -19,32 +19,49 @@ namespace HealthSystemRvisitedGageMcKenzie
     {
         int _CurrentHealth;
         int _MaxHealth;
-
+        public float GetHealth() 
+        {
+            return _CurrentHealth;
+        }
         public void TakeDamage(int damage)
         {
-
+            _CurrentHealth = _CurrentHealth - damage;
         }
         public void Restore()
         {
-
+            _CurrentHealth = _MaxHealth;
         }
 
         public void Heal(int heal)
         {
-
+            _CurrentHealth = _CurrentHealth + heal;
         }
-        Health(int maxHealth)
+        public Health(int maxHealth)
         {
+            _MaxHealth = maxHealth;
+            _CurrentHealth = maxHealth;
 
         }
     }
 
     class Player
     {
-        Health _health;
-        Health _shield;
+        Health _health = new Health(100);
+        Health _shield = new Health(50);
+        string _name;
+
+
+        
 
         void TakeDamage(int damage)
+        {
+            if (_shield.GetHealth() > 0 )
+            {
+                 = _shield.GetHealth() - damage;
+            }
+
+        }
+        string GetStatusString()
         {
 
         }
